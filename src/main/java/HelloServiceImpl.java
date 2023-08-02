dff43a75b2687787fc5bcb766302008c4a997859
Gerald Pasten <gpasten@redhat.com>
2023-08-02 11:46:09 -0400

    first commit

---
+++ src/main/java/HelloServiceImpl.java
@@ -0,0 +1,12 @@
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+
+@WebService(serviceName = "HelloService")
+public class HelloServiceImpl implements HelloService{
+    
+    @WebMethod
+    @Override
+    public String hello (String text) {
+        return "Hello " + text + "!";
+    }
+}
